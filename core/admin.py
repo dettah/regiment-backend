4075f335bdfba2792baa1138b03db3fcfb8dbb05
dettah <dettah7@gmail.com>
2026-06-16 10:58:31 +0100

    Render clean deployment

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ("name", "time", "trigger")
+
+
+@admin.register(Command)
+class CommandAdmin(admin.ModelAdmin):
+    list_display = ("command_text", "activity")
+
+
+@admin.register(LanguageGuide)
+class LanguageGuideAdmin(admin.ModelAdmin):
+    list_display = ("letter", "code_word")
+
+
+@admin.register(QuizQuestion)
+class QuizQuestionAdmin(admin.ModelAdmin):
+    list_display = ("question",)
+
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ("username", "score")
--- regiment_ai/settings.py
+++ regiment_ai/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "regiment-backend.onrender.com",
+    ".onrender.com",
 ]
 
 
